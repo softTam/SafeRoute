7cad7d55be1f1d5af66a2c4dd7525f0f96598bc5
hieuduong61 <hieuduong612001@gmail.com>
2023-10-28 22:53:44 -0700

    Report Crime Update

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,8 @@
 import sqlite3
 from flask import Flask, render_template,request,jsonify
 from flask_cors import CORS
+import requests
+APIkey = 'AIzaSyAUo1-E-SIqMp2Pq1vL4fVQKG6S36UcD7o'
 
 app = Flask(__name__)
 CORS(app)
@@ -13,8 +15,38 @@ def get_db_connection():
 # @app.route('/members')
 # def get():
 #     return {"lists": ["Member1", "Member2", "Member3"]}
+@app.route('/reportCrime', methods = ['POST'])
+def report():
+    if request.method == 'POST':
+        lat_lng = request.get_json()
+        conn = get_db_connection()
+        id = None
+        with open("primary_key_count.txt", 'r') as my_file:
+            id = str(int(my_file.readline())+1)
+        with open("primary_key_count.txt", 'w') as my_file:
+            my_file.write(str(id))
+        data = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat_lng['latitude']},{lat_lng['longtitude']}&key={APIkey}")
+        address = data['formatted_address']
+        longtitude = lat_lng['longtitude']
+        latitude = lat_lng['latitude']
+        type = 'Violent Crime'
+        
+        from datetime import date,datetime
+        today = date.today()
+        d4 = today.strftime("%b-%d-%Y")
+        date_ = d4
+        time =  datetime.now().strftime("%H:%M:%S")
+        
+        conn.execute(f"INSERT INTO crime_table(id,type,address,longtitude,latitude,date,time) VALUES ({id},'{type}','{address}','{longtitude}','{latitude}','{date_}','{time}')")
+        conn.commit()
+        conn.close()
+        
+        return "Executed"
     
-@app.route('/get')
+
+
+
+@app.route('/')
 def get():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM crime_table').fetchall()
